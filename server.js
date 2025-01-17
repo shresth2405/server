e50d5a2719ae0a02151a4cabfead46466d0f6708
Abhishek Raj <22bec006@nith.ac.in>
2025-01-17 15:56:07 +0530

    test

--- server.js
+++ server.js
@@ -1,28 +1,36 @@
 const path = require("path");
 const express = require("express");
 const WebSocket = require("ws");
+const http = require("http");
 const app = express();
 
-const WS_PORT = 8888;
-const HTTP_PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
-const wsServer = new WebSocket.Server({ port: WS_PORT }, () => console.log(`WS Server is listening at ${WS_PORT}`));
+const server = http.createServer(app);
+
+const wsServer = new WebSocket.Server({ server });
 
 let connectedClients = [];
-wsServer.on("connection", (ws, req) => {
-	console.log("Connected");
-	connectedClients.push(ws);
 
-	ws.on("message", (data) => {
-		connectedClients.forEach((ws, i) => {
-			if (connectedClients[i] == ws && ws.readyState === ws.OPEN) {
-				ws.send(data);
-			} else {
-				connectedClients.splice(i, 1);
-			}
-		});
-	});
+wsServer.on("connection", (ws) => {
+    console.log("Client connected");
+    connectedClients.push(ws);
+    ws.on("message", (data) => {
+        connectedClients.forEach((client, i) => {
+            if (connectedClients[i] === client && client.readyState === client.OPEN) {
+                client.send(data);
+            } else {
+                connectedClients.splice(i, 1);
+            }
+        });
+    });
+
+    ws.on("close", () => {
+        connectedClients = connectedClients.filter(client => client !== ws);
+    });
 });
-
 app.get("/client", (req, res) => res.sendFile(path.resolve(__dirname, "./client.html")));
-app.listen(HTTP_PORT, () => console.log(`HTTP server listening at ${HTTP_PORT}`));
+
+server.listen(PORT, () => {
+    console.log(`Server is listening at http://localhost:${PORT}`);
+});
